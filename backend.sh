59f2503213748a49e0f9071a937769ae13a4a52e
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2024-07-22 11:03:53 +0200

    common in all expense project

--- backend.sh
+++ backend.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+
+check_root
+
 echo "Please enter DB password:"
 read  mysql_root_password
 
